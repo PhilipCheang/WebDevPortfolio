880bd5dd98d594bbc45a35774ba09097ff6aec8b
[Philip Cheang] <[philipcheang7@gmail.com]>
2023-06-27 18:52:40 -0500

    update hero text

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -15,11 +15,12 @@ const Hero = () => {
         </div>
         <div>
           <h1 className={`${styles.heroHeadText}text-white`}>
-            Hi, I'm <span className="text-[#915eff]">Philip</span>
+            Hi, my name is {''}
+            <br className="sm:block hidden" />
+            <span className="text-[#915eff]">Philip Cheang</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I develop 3D visuals, user <br className="sm:block hidden" />{' '}
-            interfaces ,and web applications.
+            Front End Developer
           </p>
         </div>
       </div>
